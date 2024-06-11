c93426e042a09a03d5dfbdbe56ad9dfa630bf59c
vinccrn <v.cornee@orange.fr>
2024-06-11 17:03:53 +0200

    add model

--- src/static/js/tensorflow.js
+++ src/static/js/tensorflow.js
@@ -5,11 +5,27 @@ characters = ['0','1','2','3','4','5','6','7','8','9',
 
 var base_url = window.location.origin;
 let model;
+
+const tfn = required('@tensorflow/tfjs-node');
+
+const modelPath = '../model.json';
+const handler = tfn.io.fileSystem(modelPath);
+
 (async function(){  
     console.log("model loading...");  
-    model = await tf.loadLayersModel('______________')
+    model = await tf.loadLayersModel(handler)
     console.log("model loaded..");
 })();
+
+setInterval(async function(){     
+    var imageData = canvas.toDataURL();    
+    let tensor = preprocessCanvas(canvas); 
+    console.log(tensor)   
+    let predictions = await model.predict(tensor).data();
+    let results = Array.from(predictions);       
+    console.log(results);
+    predictShow(results)
+}, 1000000);
 
 function preprocessCanvas(image) { 
     let tensor = tf.browser.fromPixels(image)
@@ -20,16 +36,6 @@ function preprocessCanvas(image) {
         .toFloat(); 
     return tensor.div(255.0);
 }
-
-setInterval(function(){     
-    var imageData = canvas.toDataURL();    
-    let tensor = preprocessCanvas(canvas); 
-    console.log(tensor)   
-    let predictions = await model.predict(tensor).data();
-    let results = Array.from(predictions);       
-    console.log(results);
-    predictShow(results)
-}, 3000);
 
 function predictShow(data){
     var max = data[0];    
